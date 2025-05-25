d17306da814911d1fc9c7cf4c220749bfb9f0e7b
Arthur Mendonça <arthu91@gmail.com>
2025-05-25 15:45:33 -0300

    criar página quem somos

---
+++ base/app/pages/quem-somos/index.jsx
@@ -0,0 +1,13 @@
+export const QuemSomos = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <h1 className="text-4xl font-bold mb-4">Quem Somos</h1>
+      <p className="text-lg text-center max-w-2xl">
+        A Base é uma organização sem fins lucrativos que atua na promoção da
+        educação e do desenvolvimento social. Nossa missão é transformar vidas
+        por meio da educação, oferecendo oportunidades de aprendizado e
+        crescimento para crianças e jovens em situação de vulnerabilidade.
+      </p>
+    </div>
+  );
+};
