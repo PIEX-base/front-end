fde60f03382e4237527027828688ee20837d6f0d
Arthur Mendonça <arthu91@gmail.com>
2025-05-25 16:23:03 -0300

    update página quem somos

--- base/app/pages/quem-somos/index.jsx
+++ base/app/pages/quem-somos/index.jsx
@@ -1,6 +1,6 @@
 export const QuemSomos = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen ">
+    <div className="flex flex-col items-center justify-center h-screen text-black">
       <h1 className="text-4xl font-bold mb-4">Quem Somos</h1>
       <p className="text-lg text-center max-w-2xl">
         A Base é uma organização sem fins lucrativos que atua na promoção da
