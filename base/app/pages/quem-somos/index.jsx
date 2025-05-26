7c2d8d27204078744de700d67692bce0bb4406f3
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-05-25 22:25:32 -0300

    Pagina criada porem tem que melhorar a responsividade ainda da versão mobile

--- base/app/pages/quem-somos/index.jsx
+++ base/app/pages/quem-somos/index.jsx
@@ -1,10 +1,12 @@
 import Imagem1 from "../../img/home/quemsomos_imagem1.png";
+import Imagem2 from "../../img/home/quemsomos_imagem2.png";
 import Imagem3 from "../../img/home/quemsomos_imagem3.jpg";
+import Imagem4 from "../../img/home/quemsomos_imagem4.jpg";
 export const QuemSomos = () => {
   return (
     <div className="flex flex-col  min-h-screen text-black px-24 pt-13 ">
       <h1 className="text-4xl font-bold mb-4 flex justify-start">Quem Somos Nós?</h1>
-      <div className="flex flex-col md:flex-row items-center gap-8 mb-8">
+      <div className="flex flex-col-reverse md:flex-row items-center gap-8 mb-8">
         <div className="md:w-1/2">
           <h2 className="text-3xl font-bold mb-4">Nossa missão</h2>
           <p className="text-lg">
@@ -17,48 +19,70 @@ export const QuemSomos = () => {
           <img
             src={Imagem1}
             alt="imagem1"
-            className="rounded-full object-cover w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48"
+            className="rounded-full object-cover border-4 border-yellow-500 w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48"
           />
         </div>
       </div>
 
-      <h2 className="text-3xl font-bold mb-4 text-center">O que fazemos</h2>
-      <p className="text-lg text-center mb-8">
-        Nossa ONG atende hoje, principalmente, a comunidade do Rato, localizado
-        no bairro Santo Teresinha, proporcionando às crianças e aos adolescentes
-        grandes oportunidades de esporte. Nossos atletas já participaram de várias
-        competições de jiu-jitsu e todos eles trouxeram de volta casa medalhas
-        e a esperança de uma vida melhor.
-      </p>
+      <div className="flex flex-col-reverse md:flex-row items-center gap-8 mb-8">
+        <div className="ml-30 flex justify-center md:justify-start">
+          <img
+            src={Imagem2}
+            alt="imagem2"
+            className="rounded-full border-4 border-yellow-500 object-cover w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 mr-30"
+          />
+        </div>
+        <div className="md:w-1/2">
+          <h2 className="text-3xl font-bold mb-4">O que fazemos</h2>
+          <p className="text-lg">
+            Nossa ONG atende hoje, principalmente, a comunidade do Rato, localizado
+            no bairro Santo Teresinha, proporcionando às crianças e aos adolescentes
+            grandes oportunidades de esporte. Nossos atletas já participaram de várias
+            competições de jiu-jitsu e todos eles trouxeram de volta casa medalhas
+            e a esperança de uma vida melhor.
+          </p>
+        </div>
+      </div>
 
-    <div className="flex flex-col md:flex-row items-center gap-8 mb-8">
+      <div className="flex flex-col-reverse md:flex-row items-center gap-8 mb-8">
         <div className="md:w-1/2">
           <h2 className="text-3xl font-bold mb-4">Temos também</h2>
           <p className="text-lg">
-             Nossa ONG atende hoje, principalmente, a comunidade do Rato, localizado
-        no bairro Santo Teresinha, proporcionando às crianças e aos adolescentes
-        grandes oportunidades de esporte. Nossos atletas já participaram de várias
-        competições de jiu-jitsu e todos eles trouxeram de volta casa medalhas
-        e a esperança de uma vida melhor.
+            Nossa ONG atende hoje, principalmente, a comunidade do Rato, localizado
+            no bairro Santo Teresinha, proporcionando às crianças e aos adolescentes
+            grandes oportunidades de esporte. Nossos atletas já participaram de várias
+            competições de jiu-jitsu e todos eles trouxeram de volta casa medalhas
+            e a esperança de uma vida melhor.
           </p>
         </div>
         <div className="ml-30 flex justify-center md:justify-end">
           <img
             src={Imagem3}
             alt="imagem3"
-            className="rounded-full object-cover w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48"
+            className="rounded-full border-4 border-yellow-500 object-cover w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48"
           />
         </div>
       </div>
+      <div className="flex flex-col-reverse md:flex-row items-center gap-8 mb-8">
+        <div className="ml-30 flex justify-center md:justify-start">
+          <img
+            src={Imagem4}
+            alt="imagem4"
+            className="rounded-full object-cover border-4 border-yellow-500 w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 mr-30"
+          />
+        </div>
+        <div className="md:w-1/2">
+          <h2 className="text-3xl font-bold mb-4 ">Faça parte</h2>
+          <p className="text-lg">
+            Durante nossas jornadas temos contado com parceiros mantenedores que
+            tornam nossa missão possível. Pessoas vêm disponibilizando seus recursos,
+            tempo e talentos para o nosso negócio emprestado. Faça parte você também
+            e caminhe junto conosco.
+          </p>
+        </div>
+      </div>
 
-    
-      <h2 className="text-3xl font-bold mb-4 text-center">Faça parte</h2>
-      <p className="text-lg text-center mb-8">
-        Durante nossas jornadas temos contado com parceiros mantenedores que
-        tornam nossa missão possível. Pessoas vêm disponibilizando seus recursos,
-        tempo e talentos para o nosso negócio emprestado. Faça parte você também
-        e caminhe junto conosco.
-      </p>
+
     </div>
   );
 };
