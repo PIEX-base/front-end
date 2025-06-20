248ebca6b634d7bced193c08cd11ade6ea07d9aa
Arthur Mendonça <arthu91@gmail.com>
2025-06-20 19:11:37 -0300

    criar páginas como ajudar e doações

--- base/app/routes.ts
+++ base/app/routes.ts
@@ -4,4 +4,6 @@ export default [
   index("routes/home.jsx"),
   route("quem-somos", "routes/quem-somos.jsx"),
   route("projetos", "routes/projetos.jsx"),
+  route("doacoes", "routes/doacoes.jsx"),
+  route("como-ajudar", "routes/como-ajudar.jsx"),
 ] satisfies RouteConfig;
---
+++ base/app/routes/doacoes.jsx
@@ -0,0 +1,5 @@
+import { Doacoes } from "../pages/doacoes";
+
+export default function Home() {
+  return <Doacoes />;
+}
